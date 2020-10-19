1c800df615e6efe9eeef17966f82a9cb17ef1e8b
gtuk <git@gtuk.dev>
2020-10-19 11:56:33 +0200

    Add additional options and make the current ones editable

--- index.js
+++ index.js
@@ -4,7 +4,14 @@ const isPng = require('is-png');
 const pngcrush = require('pngcrush-bin');
 
 module.exports = options => buf => {
-	options = {...options};
+	options = {
+		brute: true,
+		force: true,
+		q: true,
+		reduce: false,
+		strip: false,
+		...options
+	};
 
 	if (!Buffer.isBuffer(buf)) {
 		return Promise.reject(new TypeError('Expected a buffer'));
@@ -15,15 +22,29 @@ module.exports = options => buf => {
 	}
 
 	const args = [
-		'-brute',
-		'-force',
-		'-q'
 	];
+
+	if (options.force) {
+		args.push('-force');
+	}
+
+	if (options.q) {
+		args.push('-q');
+	}
+
+	if (options.brute) {
+		args.push('-brute');
+	}
 
 	if (options.reduce) {
 		args.push('-reduce');
 	} else {
 		args.push('-noreduce');
+	}
+
+	if (options.strip) {
+		args.push('-rem');
+		args.push('alla');
 	}
 
 	args.push(execBuffer.input, execBuffer.output);
